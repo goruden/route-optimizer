b19c3ab802b6e7341159fbdfd1a127ffdf9a51e4
Norovsuren Dorjderem <norven.norov@gmail.com>
2026-03-27 16:41:18 +0800

    Update ecosystem.config.js

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,22 +1,33 @@
 module.exports = {
-  apps: [{
-    name: 'route-optimizer-frontend',
-    cwd: './',
-    script: 'node dist/main.js',
-    instances: 1,
-    autorestart: true,
-    watch: false
-  }],
+  apps: [
+    {
+      name: "route-optimizer-frontend",
+      cwd: "/opt/route-optimizer/current/frontend",
+      script: "pnpm",
+      args: "start",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ],
+
   deploy: {
     dev: {
-      user: 'ubuntu',
-      host: '54.191.84.231',
-      ref: 'origin/main',
-    //   key: '~/.ssh/qcomm-dev',
-      repo: 'https://github.com/goruden/route-optimizer.git',
-      path: '/opt/route-optimizer',
-      'pre-setup': 'sudo mkdir -p /opt/route-optimizer && sudo chown -R ubuntu:ubuntu /opt/route-optimizer',
-      'post-deploy': 'pnpm i && pnpm build && cd ./apps/route-optimizer && pm2 reload ecosystem.config.js'
+      user: "ubuntu",
+      host: "54.191.84.231",
+      ref: "origin/main",
+      repo: "https://github.com/goruden/route-optimizer.git",
+      path: "/opt/route-optimizer",
+
+      "pre-setup":
+        "sudo mkdir -p /opt/route-optimizer && sudo chown -R ubuntu:ubuntu /opt/route-optimizer",
+
+      "post-deploy":
+        "cd frontend && pnpm install && pnpm build && pm2 reload ecosystem.config.js --env dev"
     }
   }
-}
+};
