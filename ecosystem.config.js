11e71c45b7f395172ad1f3d2984913eef8716184
Norovsuren Dorjderem <norven.norov@gmail.com>
2026-03-30 11:15:20 +0800

    x

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "route-optimizer-frontend",
-      cwd: "/opt/route-optimizer/current/frontend",
+      cwd: "/opt/route-optimizer/source/frontend",
       script: "pnpm",
       args: "start",
       instances: 1,
@@ -27,8 +27,8 @@ module.exports = {
       "pre-setup":
         "sudo mkdir -p /opt/route-optimizer && sudo chown -R ubuntu:ubuntu /opt/route-optimizer",
 
-      "post-deploy":
-        "cd frontend && pnpm install && pnpm build && pm2 reload ecosystem.config.js --env dev"
+     "post-deploy":
+  "cd /opt/route-optimizer/source/frontend && pnpm install && pnpm build && pm2 reload ecosystem.config.js --env dev"
     }
   }
 };
